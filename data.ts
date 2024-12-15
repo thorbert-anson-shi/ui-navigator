443d0306fce25d8778ccc01f454c2ff444c9e895
tobtoby <tobyas2005139@gmail.com>
2024-12-16 02:37:43 +0700

    feat: created add-location and add-path endpoints; todo: integrate kv database to store values

--- data.ts
+++ data.ts
@@ -1,23 +1,25 @@
 export class Location {
-    id: string = crypto.randomUUID();
-    name: string;
-    lat: number;
-    lon: number;
+  id: string = crypto.randomUUID();
+  name: string;
+  lat: number;
+  lon: number;
 
-    constructor(name: string, lat: number, lon: number) {
-        this.name = name;
-        this.lat = lat;
-        this.lon = lon;
-    }
+  constructor(name: string, lat: number, lon: number) {
+    this.name = name;
+    this.lat = lat;
+    this.lon = lon;
+  }
 }
 
 export class Path {
-    id: string = crypto.randomUUID();
-    endpoints: Set<Location>;
+  id: string = crypto.randomUUID();
+  endpoints: Set<Location>;
+  distance: number;
 
-    constructor(loc1: Location, loc2: Location) {
-        this.endpoints = new Set<Location>();
-        this.endpoints.add(loc1);
-        this.endpoints.add(loc2);
-    }
+  constructor(loc1: Location, loc2: Location, distance: number) {
+    this.endpoints = new Set<Location>();
+    this.endpoints.add(loc1);
+    this.endpoints.add(loc2);
+    this.distance = distance;
+  }
 }
--- main.ts
+++ main.ts
@@ -1,12 +1,28 @@
-import { Hono } from 'hono';
-import { Location, Path } from './data.ts';
+import { Hono } from "hono";
+import { Location, Path } from "./data.ts";
 
-const app = new Hono()
+const app = new Hono();
 
-app.post('/add-location', async (c) => {
-    const body = await c.req.json();
-    const location = new Location(body.name, body.lat, body.lon);
-    return c.text("Location added: " + location.id);
-})
+app.post("/add-location", async (c) => {
+  const body = await c.req.json();
+  const location = new Location(body.name, body.lat, body.lon);
+  return c.text("Location added: " + location.id);
+});
 
-Deno.serve(app.fetch)
+app.post("/add-path", async (c) => {
+  const body = await c.req.json();
+  const path = new Path(body.loc1, body.loc2, body.distance);
+  return c.text("Path added: " + path.id);
+});
+
+app.notFound((c) => {
+  return c.json({
+    error: "Page not found",
+    status: 404,
+    availableEndpoints: app.routes.map((endpoint) =>
+      endpoint.method + " " + c.req.header("Host")! + endpoint.path
+    ),
+  });
+});
+
+Deno.serve(app.fetch);

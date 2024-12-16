005388bc120d2d0ad31d34fd19b17b5813a050f0
tobtoby <tobyas2005139@gmail.com>
2024-12-16 18:48:29 +0700

    minor: edited readme and main.ts

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 An API built to find the fastest route between any two points at the University
 of Indonesia.
+
+Built with [Hono](https://hono.dev/), running on the [Deno](https://deno.com/)
+JavaScript runtime.
+
+> The API Documentation is still a work in progress. Thank you for your patience
+> ^U^
--- main.ts
+++ main.ts
@@ -3,48 +3,41 @@ import { Location, Path } from "./data.ts";
 
 const app = new Hono();
 
-const kv = await Deno.openKv();
+app.get("/", (c) => c.json({
+  description: "UI Navigator is an API built to find the fastest route between any two points at the University of Indonesia.",
+  availableEndpoints: availableEndpoints
+}))
 
-app.get("/", (c) =>
-  c.json({
-    description:
-      "UI Navigator is an API built to find the fastest route between any two points at the University of Indonesia.",
-    availableEndpoints: availableEndpoints,
-  }));
-
+// TODO: Fix add-location endpoint
 app.post("/add-location", async (c) => {
   const body = await c.req.json();
   const location = new Location(body.name, body.lat, body.lon);
-  // kv.set(["location", location.lat, location.lon], location);
   return c.json(
     {
       message: "Location added successfully",
       location: location.name,
       lat: location.lat,
       lon: location.lon,
-    },
-    200,
+    }, 200
   );
 });
 
+// TODO: Fix add-path endpoint
 app.post("/add-path", async (c) => {
   const body = await c.req.json();
-  const path = new Path(body.loc1, body.loc2, body.distance);
-  // kv.set(["path", path.id], path);
+
+  const path = body.coordinates;
+
   return c.json(
     {
       message: "Path added successfully",
       id: path.id,
-      endpoints: path.endpoints.values().map((endpoint) => endpoint.name),
-      distance: path.distance,
-    },
+      distance: path.distance
+    }
   );
 });
 
-const availableEndpoints = app.routes.map((endpoint) => ({
-  method: endpoint.method,
-  path: endpoint.path,
-}));
+const availableEndpoints = app.routes.map((endpoint) => ({ method: endpoint.method, path: endpoint.path }));
 
 app.notFound((c) => {
   return c.json({
